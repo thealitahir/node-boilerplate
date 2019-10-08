7a7a3654a83f10e33f146e74470ebdb20e7c3c3e
Ali <annikhan55@gmail.com>
2019-10-09 02:46:48 +0500

    forget password in progress

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var login = require('./routes/login');
-var register = require('./routes/register');
+var user = require('./routes/userController');
 var port = 3000;
 // Configurations
 global.CONFIGURATIONS = {
@@ -10,19 +9,26 @@ global.CONFIGURATIONS = {
   dbPort: 27017,
   db: 'test',
   ssl: false,
-  projectpath: '/home/platformFrontend/numtraplatformv2',
-}
+  projectpath: '/home/platformFrontend/numtraplatformv2'
+};
 
-/* app.use('/login',login);
-app.use('/register',register); */
-var uri = 'mongodb://' + CONFIGURATIONS.dbHost + ':' + CONFIGURATIONS.dbPort + '/' + CONFIGURATIONS.db;
+app.use('/user', user);
+var uri =
+  'mongodb://' +
+  CONFIGURATIONS.dbHost +
+  ':' +
+  CONFIGURATIONS.dbPort +
+  '/' +
+  CONFIGURATIONS.db;
 mongoose.connect(uri);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log("DB CONNECTED");
-  var server = app.listen(port, '0.0.0.0', function () {
+db.once('open', function() {
+  console.log('DB CONNECTED');
+  var server = app.listen(port, '0.0.0.0', function() {
     console.log('Server started on port :' + port);
   });
   io = require('socket.io').listen(server);
 });
+
+module.exports = app;
