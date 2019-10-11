98df0c6d33815cd566a7feb90b6eae27715065fe
thealitahir431 <atahir@numtra.com>
2019-10-11 17:35:35 +0500

    adds register and login module

--- models/user.js
+++ models/user.js
@@ -5,7 +5,11 @@ var UserSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true },
+    location: { type: String },
+    image: { type: Schema.Types.Mixed },
+    phoneNo: { type: String, required: true }
+
 });
 
 module.exports = mongoose.model('Users', UserSchema);
--- routes/userController.js
+++ routes/userController.js
@@ -6,41 +6,111 @@ var http = require('https');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
 
-router.get('/login', function (req, res) {
+router.get('/login', async (req, res) => {
   console.log('in login');
   console.log(req.body);
-  UserModel.findOne(
-    { email: req.body.email, password: req.body.password },
-    function (err, user) {
-      if (!err) {
-        res.send({ status: true, message: 'User found', data: user });
-      } else {
-        res.send({ status: false, message: 'User not found' });
+  const user = await new Promise((resolve, reject) => {
+    UserModel.findOne(
+      { email: req.body.email, password: req.body.password },
+      function (err, user) {
+        if (!err) {
+          resolve(user);
+          //res.send({ status: true, message: 'User found', data: user });
+        } else {
+          reject(err);
+          // res.send({ status: false, message: 'User not found' });
+        }
       }
+    );
+  });
+  console.log(user);
+
+  if (!user) {
+    res.send({ status: false, message: 'Invalid Credentials', data: {} });
+  } else {
+    var key = generateRandomString();
+    const updated_user = await new Promise((resolve, reject) => {
+      UserModel.findOneAndUpdate(
+        { email: req.body.email },
+        {
+          $set: {
+            auth_key: key
+          }
+        },
+        { new: true },
+        function (error, auth_user) {
+          if (!error) {
+            resolve(auth_user);
+          } else {
+            reject(error);
+          }
+        }
+      );
+    });
+    console.log(updated_user);
+    if (updated_user) {
+      res.send({
+        status: true,
+        message: 'User login successful',
+        data: updated_user
+      });
+    } else {
+      res.send({ status: false, message: 'User login failed', data: {} });
     }
-  );
+  }
 });
 
-router.post('/register', function (req, res) {
+router.post('/register', async (req, res) => {
   console.log('in register');
   console.log(req.body);
-  var newUser = new UserModel();
-  newUser.firstName = req.body.fullName;
-  newUser.lastName = req.body.lastName;
-  newUser.username = req.body.email;
-  newUser.password = req.body.password;
 
-  newUser.save((err, doc) => {
-    if (!err) {
+  const unique_user = await new Promise((resolve, reject) => {
+    UserModel.findOne({ email: req.body.email }, (err, user) => {
+      if (!err) {
+        resolve(user);
+      } else {
+        reject(err);
+      }
+    });
+  });
+  console.log(unique_user);
+
+  if (unique_user) {
+    res.send({ status: false, message: 'Email already exists', data: {} });
+  } else {
+    var key = generateRandomString();
+    var newUser = new UserModel();
+    newUser.firstName = req.body.firstName;
+    newUser.lastName = req.body.lastName;
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
+    newUser.phoneNo = req.body.phoneNo;
+    newUser.location = req.body.location;
+    newUser.auth_key = key;
+    const new_user = await new Promise((resolve, reject) => {
+      newUser.save((err, new_user) => {
+        if (!err) {
+          resolve(new_user);
+        } else {
+          reject(err);
+        }
+      });
+    });
+    console.log(new_user);
+    if (new_user) {
       res.send({
         status: true,
         message: 'User registered successfully',
-        data: doc
+        data: new_user
       });
     } else {
-      res.send({ status: false, message: 'User not found' });
+      res.send({
+        status: false,
+        message: 'Unable to register user',
+        data: {}
+      });
     }
-  });
+  }
 });
 
 router.post('/forgetPassword', async (req, res) => {
@@ -146,5 +216,15 @@ router.post('/asycAwaitExample', (req, res) => {
     }
   );
 });
+
+function generateRandomString() {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (var i = 0; i < 25; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  text += Date.now();
+  return text;
+}
 
 module.exports = router;
