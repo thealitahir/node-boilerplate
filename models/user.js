cc9f5afc91024d0db2581eac8069f19b7a8b8246
Ali <annikhan55@gmail.com>
2019-10-15 22:32:01 +0500

    resolves auth_token issue

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    location: { type: String },
-    image: { type: Schema.Types.Mixed },
-    phoneNo: { type: String, required: true }
-
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  location: { type: String },
+  image: { type: Schema.Types.Mixed },
+  phoneNo: { type: String, required: true },
+  auth_key: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Users', UserSchema);
