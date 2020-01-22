db157259a0a9294f2747c4c44e4654fac89e1c90
thealitahir <ali.tahir@Cdocs.local>
2020-01-23 00:54:38 +0500

    adds .env part

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const user = require('./routes/userController');
 const product = require('./routes/productController');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
-var port = 3010;
+require('dotenv').config();
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,37 +17,22 @@ app.use(bodyParser.json());
 
 app.use(cookieParser(''));
 // Configurations
-global.CONFIGURATIONS = {
-  dbHost: 'localhost',
-  dbPort: 27017,
-  db: 'test',
-  ssl: false,
-  projectpath: '/home/platformFrontend/numtraplatformv2'
-};
-
 app.use('/user', user);
 app.use('/product',product)
-var uri =
-  'mongodb://' +
-  CONFIGURATIONS.dbHost +
-  ':' +
-  CONFIGURATIONS.dbPort +
-  '/' +
-  CONFIGURATIONS.db;
 
 var opt = {
   useNewUrlParser: true
 };
 mongoose.connect(
-  'mongodb+srv://thealitahir:testuser@cluster0-qbtub.azure.mongodb.net/test?retryWrites=true&w=majority',
+  process.env.CONNECTION_STRING,
   opt
 );
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('DB CONNECTED');
+  console.log('DB CONNECTED', process.env.PORT);
   var server = app.listen(process.env.PORT || 8080, process.env.HOST, function() {
-    console.log('Server started on port :' + port);
+    console.log('Server started on port :' + process.env.PORT);
   });
   io = require('socket.io').listen(server);
 });
